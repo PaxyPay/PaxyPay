5478c211fbd3071fe3075981ed05b5d5ad0cc600
angelo friello <angelofriello01@gmail.com>
2024-05-10 16:55:20 +0200

    fix lenguage on index

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'welcome' => 'Welcome on my site!',
+   
+];
+?>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,8 +15,12 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/bce4c6f505.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{ env('APP_URL') }}/build/assets/app.b0bef6e8.css">  
-    <style>td{color: currentColor !important}</style>
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/build/assets/app.b0bef6e8.css">
+    <style>
+        td {
+            color: currentColor !important
+        }
+    </style>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     {{-- <script type="module" src="{{ env('APP_URL') }}/build/assets/app.494c25bb.js" onload=""></script> --}}
 </head>
@@ -34,7 +38,9 @@
                     {{-- config('app.name', 'Laravel') --}}
                 </a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -50,36 +56,41 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">Login</a>
-                        </li>
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">Register</a>
-                        </li>
-                        @endif
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('messages.accedi') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('messages.registrati') }}</a>
+                                </li>
+                            @endif
                         @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                
-                                <a class="dropdown-item" href="{{ route('admin.payment.index') }}">Pagamenti</a>
-                                <a  class="dropdown-item" href="{{route('profile.settings')}}">Settaggi</a>
-                                <a class="dropdown-item" href="{{ url('profile') }}">Profilo</a>
-                                <a class="dropdown-item" href="{{ url('profile/dashboard') }}">Dashboard</a>
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    Logout
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                                    <a class="dropdown-item"
+                                        href="{{ route('admin.payment.index') }}">{{ __('messages.pagamenti') }}</a>
+                                    <a class="dropdown-item"
+                                        href="{{ route('profile.settings') }}">{{ __('messages.settaggi') }}</a>
+                                    <a class="dropdown-item" href="{{ url('profile') }}">{{ __('messages.profilo') }}</a>
+                                    <a class="dropdown-item"
+                                        href="{{ url('profile/dashboard') }}">{{ __('messages.statistiche') }}</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('messages.esci') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
                         @endguest
                     </ul>
                 </div>
@@ -118,4 +129,6 @@
             document.documentElement.classList.remove('loaded');
         });
 </script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+</script>
