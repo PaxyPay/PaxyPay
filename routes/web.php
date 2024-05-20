fe9b0c64322fcdb7b2626446a028a3709bb78c7f
angelo friello <angelofriello01@gmail.com>
2024-05-20 14:39:02 +0200

    4

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get()
+    // Route::get()
 });
 
 Route::middleware('auth','verified')->group(function () {
